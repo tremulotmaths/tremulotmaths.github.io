3df4a416b4f2caf027fd00cb0de1b806fdf49699
dom <tremulot.maths@gmail.com>
2023-05-11 00:11:53 +0200

    Ajout du minisite 3 + commentaires

---
+++ minisites2023/site3/script.js
@@ -0,0 +1,28 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const textSpans = document.querySelectorAll('p span');
+const devText = document.querySelector('dev-text');
+const btns = document.querySelectorAll('.btn-first');
+const logo = document.querySelector('.logo');
+const medias = document.querySelectorAll('.bulle');
+const l1 = document.querySelector('.l1');
+const l2 = document.querySelector('.l2');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .staggerFrom(textSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .staggerFrom(devText, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, '-=2')
+    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
+
+    
+    
+
+    TL.play();
+})
