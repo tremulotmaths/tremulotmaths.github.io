1d51630458bacc129053f80f4da5178f8c6f73db
tremulotmaths <tremulot.maths@gmail.com>
2026-01-28 21:44:46 +0100

    Formulaire note au bac
    Mise à jour de la version élève

--- formulairenotebac/script.js
+++ formulairenotebac/script.js
@@ -6,12 +6,11 @@ function calcul(){
 		let elmts = formulaire.elements; //"elmts" contient le tableau des éléments de formulaire
 				
 		let hg_1 = parseFloat(elmts[0].value); // on stocke dans 16 variables la valeur entrée dans les éléments 0, 1,... 15 du formulaire...
-		let lva_1 = parseFloat(elmts[1].value); // ...en pensant à les transtyper en nombres flottants !
-		// ...à continuer
+		// à poursuivre
 		
-		let moyenne = ; // calcul de la moyenne...à compléter
+		let moyenne =  // calcul de la moyenne...à écrire
 		
-		elmt = document.getElementById("note_finale"); // sélection de l'élément "note_finale"
-		elmt.innerHTML = Math.round(100*moyenne)/100; // affectation du contenu texte
+		 // sélection de l'élément "note_finale"
+		 // affectation du contenu texte
 }
 
